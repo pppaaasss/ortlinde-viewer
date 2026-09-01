7292bb8baee9ed4a1cb9e8affa0aba1614ba6399
pppaaasss <45713266+pppaaasss@users.noreply.github.com>
2026-09-02 00:32:28 +0800

    feat: rebuild gallery browsing (public/sw.js)

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ortlinde-viewer-v2";
+const CACHE_NAME = "ortlinde-viewer-v3";
 const APP_SHELL = ["/manifest.webmanifest", "/icon.svg"];
 
 self.addEventListener("install", (event) => {
