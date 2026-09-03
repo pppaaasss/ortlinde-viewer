9bd0f336c100672b81ee45efdd727a0a72565794
pppaaasss <45713266+pppaaasss@users.noreply.github.com>
2026-09-03 08:17:41 +0800

    Finalize documented gallery flow: public/sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ortlinde-viewer-v4";
+const CACHE_NAME = "ortlinde-viewer-v5";
 const APP_SHELL = ["/manifest.webmanifest", "/icon.svg"];
 
 self.addEventListener("install", (event) => {
